15d2727484eb0730f878955286617e0d80510bc1
markkizz <mark.pattanapara@gmail.com>
2021-01-06 20:49:01 +0700

    feat(setup new machine): Define install step

--- setup.sh
+++ setup.sh
@@ -1,3 +1,34 @@
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [[ $machine == *"UNKNOWN"* ]]; then
+  echo "Unknown operation system. Please try again later."
+  exit 1
+fi
+
+## install essential packages ##
+echo "Step: Install essential packages. Please stay focus in your terminal."
+if [[ $machine == *"Mac"* ]]; then
+  echo "Essential: Install Homebrew..."
+  mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+fi
+
+if [[ $machine == *"Linux"* ]]; then
+  echo "Essential: Install build-essential..."
+  sudo apt-get update
+  sudo apt-get install build-essential -y
+fi
+echo "Done Essential!"
+################################
+
+
+
 echo "Step: Install fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
@@ -6,4 +37,22 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 echo "Step: Installing oh_my_zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "Step: Install zsh theme"
+if [[ $machine == *"Mac"* ]]; then
+  brew install romkatv/powerlevel10k/powerlevel10k
+  echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+else
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 
+echo "Step: Copying .zshrc"
+cp .zshrc ~/.zshrc
+
+mkdir ~/opt
+mkdir ~/opt/bin
+cp ./bin/* ~/opt/bin
+
+echo "Step: Sourcing zsh"
+source ~/.zshrc
+
+echo "Done!!"
