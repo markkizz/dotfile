584540ec603bcfc4ce9767755dd3b17ab122b1c2
markkizz <mark.pattanapara@gmail.com>
2021-01-23 20:35:09 +0700

    feat(linux setup): Add Docker installation

---
+++ linux/03-docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Installing Docker"
+
+echo "--- Setup docker Repository"
+# 1.1
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+# 1.2
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+# 1.3
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "--- Installing Docker Engine"
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io
