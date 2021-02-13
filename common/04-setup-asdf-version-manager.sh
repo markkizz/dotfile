e3d3b57d310344f66f51c5b9c6691bf649a874c2
markkizz <mark.pattanapara@gmail.com>
2021-02-13 18:18:03 +0700

    feat: Add asdf runtime manager

---
+++ common/04-setup-asdf-version-manager.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# https://github.com/asdf-vm/asdf
+
+setup_asdf() {
+  if ! test -e ~/.asdf
+  then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8 --quiet &> /dev/null
+  else
+    echo "asdf is already installed!"
+  fi
+
+  # HACK: add asdf to PATH!
+  echo "export PATH=\"\$PATH:$HOME/.asdf/bin\"" >> ~/.zshrc
+  source ~/.zshrc
+
+  if ! command -v asdf
+  then
+    echo "asdf binary is not found."
+    return
+  fi
+
+  echo "🌟 Updating asdf to latest stable version..."
+
+  # Update asdf to the latest stable version.
+  asdf update
+
+  for plugin in java nodejs python ruby yarn
+  do
+    echo "ℹ️ Adding $plugin plugin..."
+    asdf plugin add $plugin
+  done
+
+  asdf plugin add golang https://github.com/kennyp/asdf-golang.git
+}
+
+if command -v asdf &> /dev/null
+then
+  echo "👍 asdf is already installed."
+else
+  echo "🌟 Installing asdf package manager..."
+  setup_asdf
+  echo "✅ installed asdf!"
+fi
