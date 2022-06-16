271637d5aa1afec2d6c17673eff7fedf56a39c49
markkizz <mark.pattanapara@gmail.com>
2022-06-16 18:03:52 +0700

    feat: update homebrew macos

--- macos/01-homebrew.sh
+++ macos/01-homebrew.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
 echo " 📦 Installing Homebrew..."
-yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+yes | mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 echo "✅ Homebrew installed!"
